4993c7c289c73b114cbb1a32acae79d01949036c
juliwhite <julietawhite30@gmail.com>
2025-10-06 19:34:59 -0600

    change deprecated options on db file

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,16 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      serverApi: {
+        version: '1',
+        strict: true,
+        deprecationErrors: true,
+      },
+      //useNewUrlParser: true,
+      //useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${"Successfull connection"}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
